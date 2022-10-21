7a76dde752aee1e52e5081ef45f9919d4636e74f
Matute <matiasdentoni@gmail.com>
2022-10-21 17:08:56 -0300

    modif para diego

--- src/db.js
+++ src/db.js
@@ -3,8 +3,23 @@ const { Sequelize, Op } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, PORT, FRONT
+  DB_USER, DB_PASSWORD, DB_HOST, PORT, FRONT, DB_NAME
 } = process.env;
+
+// // segun la piba
+//
+// const { DATABASE_URL } = process.env;
+//
+// const sequelize = new Sequelize(DATABASE_URL, {
+//   logging: false, // set to console.log to see the raw SQL queries
+//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed}
+//   // configuración adicional HEROKU
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false
+//     }
+//   },
 
 
 //Para Node localhost -----
@@ -14,19 +29,35 @@ const {
 // });
 
 //Para HEROKU  -----
-const { DATABASE_URL } = process.env;
 
-const sequelize = new Sequelize(DATABASE_URL, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed}
-  // configuración adicional HEROKU
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  },
-});
+let sequelize =
+  process.env.NODE_ENV === "production"
+    ? new Sequelize({
+        database: DB_NAME,
+        dialect: "postgres",
+        host: DB_HOST,
+        port: 5432,
+        username: DB_USER,
+        password: DB_PASSWORD,
+        pool: {
+          max: 3,
+          min: 1,
+          idle: 10000,
+        },
+        dialectOptions: {
+          ssl: {
+            require: true,
+            // Ref.: https://github.com/brianc/node-postgres/issues/2009
+            rejectUnauthorized: false,
+          },
+          keepAlive: true,
+        },
+        ssl: true,
+      })
+    : new Sequelize(
+        `postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_HOST}/${DB_NAME}`,
+        { logging: false, native: false }
+      );
 
 const basename = path.basename(__filename);
 
